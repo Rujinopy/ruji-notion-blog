be9a5c7cff4d652c4d640145cc897a849d78da00
Rujinopy <ruth3442@gmail.com>
2023-10-29 00:47:29 +0700

    module updated

--- next.config.js
+++ next.config.js
@@ -13,5 +13,12 @@ module.exports = {
       'res.craft.do',
       'res.cloudinary.com'
     ]
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
 };
